11cded836dd6dcf32596f25a3e2788222666b365
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-06-27 14:41:32 -0400

    Add project files.

---
+++ Blinds/Models/BlindsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blinds.Models
+{
+    public class BlindsDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BlindsDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        
+        public static BlindsDbContext Create()
+        {
+            return new BlindsDbContext();
+        }
+    }
+}
---
+++ Blinds/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Blinds.Startup))]
+
+namespace Blinds
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
+        }
+    }
+}
